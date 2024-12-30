ef0c8ddaf1dbb4f4a147cb390bd7ba7c9fe16139
George Petras <petras@acm.org>
2024-12-30 17:16:41 -0500

    minor changes LaunchUbuntuApp.ps1

--- LaunchUbuntuApp.ps1
+++ LaunchUbuntuApp.ps1
@@ -1,4 +1,9 @@
-
+#
+# Ubuntu is not a typical WindowsApp with an App.xbf file in its app folder:
+# C:\Program Files\WindowsApps\<app-name-and-id>\
+# It actually has ubuntu.exe there, which means instead of appending !App to the
+# Ubuntu PackageFamilyName you must append "!ubuntu"
+#
 $appname="Ubuntu"
 $appnameTarget="ubuntu"
 write-output "Will launch Ubunutu"
