1bfb36cc6294db986a24cc78d42ada03525aa539
G. P. <petras@acm.org>
2025-04-23 16:20:53 -0400

    Update FixDynamicLighting.ps1
    Added a better  header comment.

--- FixDynamicLighting.ps1
+++ FixDynamicLighting.ps1
@@ -1,6 +1,16 @@
 #
-# Disable and re-enable dynamic lighting to force the proper state
-# This will be triggered via Task Manager when the system resumes from sleep
+# Asrock has a motherboard RGB driver that supports Windows 11 Dynamic Lighting.
+# It basically works except when the computer wakes from sleep, the motherboard
+# lighting isn't on even  though the Dynamic Lighting shows as being on.
+# This script just disables and re-enables the registry entry for Dynamic Lighting
+# 
+# Using Task Scheduler and add a task trigger "On an event", Basic, Log: "System",
+# Source: "Kernel-Power", Event ID: "566"
+# A task delay of 1 minute might be good, but may not be necessary. 
+# The task "Action" should be "Start a program".  To reduce the window "flash" that
+# occurs when running a PowerShell script you can set "Program/script:" to
+# "cmd" and "Add arguments (optional):" to:
+#  '/c start /min "" pwsh.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File <Path-To>\FixDynamicLighting.ps1'
 #
 #
 # Registry disable dynamic lighting
