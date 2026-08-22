3b437ab2ae66d8bf7fe524b8e00dff7402be8f64
George Petras <petras@acm.org>
2026-08-21 22:09:04 -0400

    small (old) updates

--- LaunchUbuntuApp.ps1
+++ LaunchUbuntuApp.ps1
@@ -4,7 +4,7 @@
 # It actually has ubuntu.exe there, which means instead of appending !App to the
 # Ubuntu PackageFamilyName you must append "!ubuntu"
 #
-$appname="Ubuntu"
-$appnameTarget="ubuntu"
+$appname="CanonicalGroupLimited"
+$appnameTarget="Ubuntu"
 write-output "Will launch Ubunutu"
-Start-Process shell:$("AppsFolder\" + (get-appxpackage | ?{$_.PackageFamilyName -like "*$appname*"}).PackageFamilyName + "!"+$appnameTarget)
+Start-Process shell:$("AppsFolder\" + (Get-AppXPackage | ?{$_.PackageFamilyName -like "*$appname*"}).PackageFamilyName + "!"+$appnameTarget)
--- StartGitDaemon.ps1
+++
@@ -1,2 +0,0 @@
-echo "Starting git daemon"
-git daemon --reuseaddr --base-path=d:\git-config-repos d:\git-config-repos --export-all --enable=receive-pack
